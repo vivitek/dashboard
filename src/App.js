1277568cb583c717e41bc15463f2743832891318
Matteo <matteo.gassend@epitech.eu>
2020-07-23 09:53:14 +0200

    [del] clean slate

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { Provider } from 'react-redux'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import store from './store'
-
-import Main from './components/Layout/Main'
-
-import Home from './components/Home'
-import Register from './components/Auth/Register'
-import Login from './components/Auth/Login'
-
-class App extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <div>
-          <BrowserRouter>
-            <Main>
-              <Switch>
-                <Route exact path="/" component={Home}/>
-                <Route path="/login" component={Login}/>
-                <Route path="/register" component={Register}/>
-              </Switch>
-            </Main>
-          </BrowserRouter>
-        </div>
-      </Provider>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-const Home = () => (
-    <div>
-        This is Home Page
-    </div>
-)
-
-export default Home
