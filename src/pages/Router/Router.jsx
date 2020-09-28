bd7f1defb4407d80ccf073ab16f28b7ca1c94f96
Matteo Gassend <matteo.gassend@epitech.eu>
2020-09-28 11:16:52 +0200

    [add]: added fade in animation to router card

--- src/pages/Router/Router.jsx
+++ src/pages/Router/Router.jsx
@@ -1,23 +1,43 @@
 import React, { useState } from 'react'
-import { Col } from 'reactstrap'
+import { Col, Row } from 'reactstrap'
 import RouterCard from './RouterCard'
 import useError from '../../hooks/useErrors'
+import { motion } from "framer-motion"
+import { ANIMATION_VARIANTS } from '../../utils/constants'
+
+
 
 const Router = () => {
-    const [routers, setRouters] = useState([{
+    const [routers, setRouters] = useState([
+    {
         name: "test",
         id: "1"
-    }])
+    }, {
+        name:"test2",
+        id: "2"
+    },
+    {
+        name: "test3",
+        id: "3"
+    }, {
+        name:"test4",
+        id: "4"
+    }
+    ])
     const errors = useError()
     return (
-        <div>
+        <React.Fragment>
             {errors}
-            {routers.map((r) => (
-                <Col xs="12" md="4" key={r._id}>
-                    <RouterCard router={r} />
-                </Col>
-            ))}
-        </div>
+            <Row>
+                {routers.map((r) => (
+                    <Col xs="12" md="3" sm="12" key={r._id}>
+                        <motion.div initial="hidden" animate="visible" variants={ANIMATION_VARIANTS} whileHover={{scale: 1.1}}>
+                            <RouterCard router={r} />
+                        </motion.div>
+                    </Col>
+                ))}
+            </Row>
+        </React.Fragment>
     )
 }
 
