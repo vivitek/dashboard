3a0d2448422c87187a5d1ccbb829b0db16404e20
Matteo Gassend <matteo.gassend@epitech.eu>
2021-01-19 11:36:07 +0100

    [add] added apollo package + helpers

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import { Container } from 'reactstrap'
-import Header from './components/Header';
-import Routes from './Routes';
-import UserContext from './contexts/UserContext';
-import RouterProvider from './contexts/RouterContext';
-import BeamsProvider from './contexts/BeamsContext';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Container } from "reactstrap";
+import Header from "./components/Header";
+import Routes from "./Routes";
+import UserContext from "./contexts/UserContext";
+import BeamsProvider from "./contexts/BeamsContext";
+import { client } from "./utils/apollo";
+import { ApolloProvider } from "@apollo/client";
 function App() {
-  const [user, setUser] = useState({})
+    const [user, setUser] = useState({});
 
-  useEffect(() => {
-
-
-
-    if (localStorage.getItem("vivi-user")) {
-      setUser(JSON.parse(localStorage.getItem("vivi-user")))
-    }
-  }, [])
-  return (
-    <BrowserRouter>
-      <UserContext value={{ user, updateUser: setUser }}>
-        <Container fluid>
-          <BeamsProvider>
-          <Header />
-          <Container fluid>
-            <RouterProvider>
-              <Routes />
-            </RouterProvider>
-          </Container>
-          </BeamsProvider>
-        </Container>
-      </UserContext>
-    </BrowserRouter>
-  );
+    useEffect(() => {
+        if (localStorage.getItem("vivi-user")) {
+            setUser(JSON.parse(localStorage.getItem("vivi-user")));
+        }
+    }, []);
+    return (
+        <BrowserRouter>
+            <main>
+                <UserContext value={{ user, updateUser: setUser }}>
+                    <Container fluid style={{ height: "100%" }}>
+                        <BeamsProvider>
+                            <Header />
+                            <ApolloProvider client={client}>
+                                <Routes />
+                            </ApolloProvider>
+                        </BeamsProvider>
+                    </Container>
+                </UserContext>
+            </main>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/utils/apollo.js
@@ -0,0 +1,107 @@
+import {
+    ApolloClient,
+    gql,
+    HttpLink,
+    InMemoryCache,
+    split,
+} from "@apollo/client";
+import { BASE_URL, BASE_WS } from "./constants";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+    uri: `${BASE_URL}/graphql`,
+});
+
+const wsLink = new WebSocketLink({
+    uri: `${BASE_WS}/graphql`,
+    options: {
+        reconnect: true,
+    },
+});
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === "OperationDefinition" &&
+            definition.operation === "subscription"
+        );
+    },
+    wsLink,
+    httpLink
+);
+
+const client = new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+});
+
+const GET_ROUTERS = gql`
+    query {
+        getRouters {
+            _id
+            name
+            url
+        }
+    }
+`;
+
+const GET_ROUTER = gql`
+    query($routerId: String!) {
+        getRouter(id: $routerId) {
+            _id
+            url
+            name
+        }
+    }
+`;
+
+const CREATE_ROUTER = gql`
+    mutation($routerCreateData: RouterCreationInput!) {
+        createRouter(createRouterData: $routerCreateData) {
+            _id
+            url
+            name
+        }
+    }
+`;
+
+const GET_BANS_FOR_ROUTER = gql`
+    query($routerId: String!) {
+        getBans(id: $routerId) {
+            address
+            banned
+            _id
+        }
+    }
+`;
+
+const UPDATE_BAN = gql`
+    mutation($banUpdate: BanUpdate!) {
+        updateBan(banUpdateData: $banUpdate) {
+            _id
+            banned
+            address
+        }
+    }
+`;
+
+const ON_BAN_CREATED = gql`
+    subscription($routerId: String!) {
+        banCreated(routerSet: $routerId) {
+            address
+            _id
+        }
+    }
+`;
+
+export {
+    client,
+    GET_ROUTERS,
+    GET_ROUTER,
+    CREATE_ROUTER,
+    GET_BANS_FOR_ROUTER,
+    UPDATE_BAN,
+    ON_BAN_CREATED,
+};
