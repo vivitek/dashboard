fff3d533dbe725e9a7d9439f4b5ac1226c8185bc
Matteo Gassend <matteo.gassend@epitech.eu>
2021-01-19 11:40:37 +0100

    [fix] linting

--- src/errors.js
+++ src/errors.js
@@ -1,5 +1,7 @@
-export default {
-	"authentication":"Authentication required",
-	"disconnected":"You have been logged out",
-	"router":"You need to select a router"
-}
+const errors = {
+    authentication: "Authentication required",
+    disconnected: "You have been logged out",
+    router: "You need to select a router",
+};
+
+export default errors;
