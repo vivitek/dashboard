0e143a3bcc1e1a9e1bf156d5916e2a66d86c4b84
Matteo Gassend <matteo.gassend@epitech.eu>
2021-02-18 11:36:28 +0100

    [add] added 404 page

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import Register from "./pages/Register";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -9,6 +8,7 @@ import Router from "./pages/Router/Router";
 import Landing from "./pages/Landing";
 import RouterDetails from "./pages/Router/RouterDetails";
 import PdfTableExport from "./pages/PdfTableExport";
+import NotFound from "./pages/NotFound";
 
 const AuthedRoute = ({ path, exact, component }) => {
     if (path === "/" && !localStorage.getItem("vivi-jwt")) {
@@ -29,9 +29,8 @@ const Routes = () => (
         <AuthedRoute path="/routers" exact component={Router} />
         <AuthedRoute path="/routers/:id" exact component={RouterDetails} />
         <AuthedRoute path="/pdf/table" exact component={PdfTableExport} />
-        <Route path="/welcome" exact component={Landing} />
-        <Route path="/register" exact component={Register} />
         <Route path="/login" exact component={Login} />
+        <Route component={NotFound} />
     </Switch>
 );
 
