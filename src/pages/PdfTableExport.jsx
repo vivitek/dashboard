3f4697978d64f63fbcf9157be21fdb7be3fb0e86
Dythic <quentin.henry@epitech.eu>
2021-03-04 14:45:31 +0100

    [add] New PDF table

--- src/pages/PdfTableExport.jsx
+++ src/pages/PdfTableExport.jsx
@@ -19,105 +19,14 @@ import {
 import React from "react";
 import { useLocation } from "react-router-dom";
 
-const styles = StyleSheet.create({
-    table: { 
-        display: "table", 
-        width: "auto", 
-        borderStyle: "none", 
-        borderWidth: 1, 
-        borderRightWidth: 0,
-        borderBottomWidth: 0 
-    }, 
-    tableRow: { 
-        margin: "auto", 
-        flexDirection: "row" 
-    },
-
-    tableCol: { 
-        width: "25%", 
-        borderStyle: "none", 
-        borderWidth: 1, 
-        borderLeftWidth: 0, 
-        borderTopWidth: 0 }, 
-        
-    tableCell: { 
-        margin: "auto", 
-        marginTop: 5, 
-        fontSize: 10 
-    }
-});
-
 const PdfTableExport = () => {
     const location = useLocation();
     const headers = JSON.parse(location.state.headers);
     const rows = JSON.parse(location.state.rows);
     const title = location.state.title;
-    console.log(rows);
 
     return (
         <PDFViewer style={{ height: "100%", width: "100%" }}>
-            <Document
-                title={title}
-                author="ViVi"
-                creator="ViVi"
-                producer="ViVi"
-                subject={title}
-            >
-            <Page size="A4">
-                <View
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                        justifyContent: "space-between",
-                        flexDirection: "row",
-                        margin: "20px",
-                    }}
-                >
-                    <Image
-                        src="/vivi_no-text.png"
-                        style={{ width: "30%" }}
-                    />
-                    <Text>{new Date().toLocaleDateString()}</Text>
-                </View>
-                <View style={{ textAlign: "left", marginLeft: "20px" }}>
-                    <Text>{title}</Text>
-                </View>
-                <View style={styles.table}> 
-                    
-                    <View style={styles.tableRow}> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>Product</Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>Type</Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>Period</Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>Price</Text> 
-                        </View> 
-                    </View> 
-                    
-                    <View style={styles.tableRow}> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>React-PDF</Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>3 User </Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>2019-02-20 - 2020-02-19</Text> 
-                        </View> 
-                        <View style={styles.tableCol}> 
-                            <Text style={styles.tableCell}>5€</Text> 
-                        </View> 
-                    </View> 
-                </View>
-            </Page>
-            </Document>
-        </PDFViewer>
-        /*<PDFViewer style={{ height: "100%", width: "100%" }}>
             <Document
                 title={title}
                 author="ViVi"
@@ -146,18 +55,33 @@ const PdfTableExport = () => {
                     </View>
                     <View style={{ margin: "20px" }}>
                         <Table data={rows}>
-                            <TableHeader textAlign={"center"} Color={"blue"}>
+                            <TableHeader 
+                                textAlign={"center"} 
+                                Color={"blue"} 
+                                isHeader={true}
+                                includeLeftBorder={false} 
+                                includeRightBorder={false} 
+                                includeTopBorder={false}
+                            >
                                 {headers
                                     .filter((e) => e.export)
                                     .map((e) => (
-                                        <TableCell weighting={0.3}>
+                                        <TableCell 
+                                            weighting={0.3}
+                                            fontSize={"20"}
+                                        >
                                             <Text style={{ marginLeft: "5px" }}>
                                                 {e.name}
                                             </Text>
                                         </TableCell>
                                     ))}
                             </TableHeader>
-                            <TableBody>
+                            <TableBody 
+                                includeLeftBorder={false} 
+                                includeRightBorder={false}
+                                includeBottomBorder={false} 
+                                includeTopBorder={false}
+                            >
                                 {headers
                                     .filter((e) => e.export)
                                     .map((p, i) => (
@@ -180,7 +104,7 @@ const PdfTableExport = () => {
                     </View>
                 </Page>
             </Document>
-        </PDFViewer>*/
+        </PDFViewer>
     );
 };
 
