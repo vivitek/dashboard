9eccd0d59ff867cee04514c5f2c6f02aa5172107
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-20 16:09:05 +0200

    [add] reworked login page to make it readable on tablets

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -44,87 +44,79 @@ const Login = () => {
   });
 
   return (
-    <div
-      className="h-full w-full bg-cover bg-center "
-      style={{
-        backgroundImage: "url(https://source.unsplash.com/random/1920x1080)",
-      }}
-    >
-      <div style={{ background: "rgba(0,0,0,0.85)" }} className="w-full h-full">
-        <div className="h-full w-full grid grid-cols-1 md:grid-cols-3">
-          <div className="hidden md:flex flex-col justify-center items-center">
-            <img
-              src="/vivi_white.svg"
-              alt="ViVi logo"
-              className="w-1/3 h-auto"
-            />
-            <div align="right" className="w-1/3">
-              <p className="mt-2 text-xl montserrat text-white">Login</p>
-            </div>
+    <div className="h-full w-full bg-cover bg-center" style={{
+      backgroundImage: "url(https://source.unsplash.com/random/1920x1080)",
+    }}>
+      <div className="w-full h-full flex flex-col md:flex-row" style={{ background: "rgba(0,0,0,0.85)" }}>
+        <div className="w-full md:w-1/3 h-1/3 md:h-full flex flex-col justify-center items-center">
+          <img
+            src="/vivi_white.svg"
+            alt="ViVi logo"
+            className="w-1/3 md:w-1/2 h-auto"
+          />
+          <div align="right" className="w-1/3 hidden md:block">
+            <p className="mt-2 text-xl montserrat text-white">Login</p>
           </div>
-          <div className="hidden md:block"></div>
-          <div className="flex flex-col justify-center h-full w-full md:pr-52 md:pl-5">
-            <div className="h-full md:h-4/6  w-full dark:bg-[#292E40] bg-white rounded-3xl">
-              <div className="px-8 py-20 h-full grid grid-cols-1">
-                <form onSubmit={formik.handleSubmit}>
-                  <div>
-                    <h1 className="font-bold text-3xl itc mt-4">
-                      {t("login.prompt")}
-                    </h1>
-                    <p className="text-xl font-semibold montserrat">
-                      {t("login.details")}
-                    </p>
+        </div>
+        <div className="w-full md:w-2/3 h-2/3 md:h-full md:flex flex-col justify-center items-center">
+          <div className="dark:bg-[#292E40] bg-white rounded-3xl py-2 px-4  w-full md:w-2/3 lg:w-1/3">
+            <form>
+              <div className="w-full flex flex-col">
+                <div className="">
+                  <h1 className="font-bold text-3xl itc">
+                    {t("login.prompt")}
+                  </h1>
+                  <p className="text-xl font-semibold montserrat">
+                    {t("login.details")}
+                  </p>
+                </div>
+                <div className="w-full flex flex-col mb-10 mt-10">
+                  <label className="montserrat mb-2">Email</label>
+                  <input
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    name="email"
+                    type="email"
+                    className="bg-gray-200 dark:bg-[#313E68] border-none rounded-xl"
+                  />
+                </div>
+                <div className="w-full flex flex-col mb-2">
+                  <label className="montserrat mb-2">Password</label>
+                  <input
+                    onChange={formik.handleChange}
+                    onBlur={formik.handleBlur}
+                    name="password"
+                    type="password"
+                    className="bg-gray-200 dark:bg-[#313E68] border-none rounded-xl"
+                  />
+                </div>
+                <div align="right" className="">
+                  <a
+                    href="mailto:contact@vincipit.com"
+                    className="montserrat "
+                    rel="noreferrer"
+                    target="_blank"
+                  >
+                    Forgotten your password?
+                  </a>
+                </div>
+                <div className="w-full flex justify-end mt-8">
+                  <div className="flex flex-col justify-center w-full">
+                    <button
+                      type="submit"
+                      className="dark:bg-[#313E68] bg-[#1473E6]  text-white px-6 py-2 rounded-lg font-medium  hover:bg-blue-600 transition duration-200 each-in-out montserrat"
+                    >
+                      {t("common.submit")}
+                    </button>
                   </div>
-                  <div>
-                    <div className="w-full flex flex-col mb-10 mt-10">
-                      <label className="montserrat mb-2">Email</label>
-                      <input
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        name="email"
-                        type="email"
-                        className="bg-gray-200 dark:bg-[#313E68] border-none rounded-xl"
-                      />
-                    </div>
-                    <div className="w-full flex flex-col mb-2">
-                      <label className="montserrat mb-2">Password</label>
-                      <input
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        name="password"
-                        type="password"
-                        className="bg-gray-200 dark:bg-[#313E68] border-none rounded-xl"
-                      />
-                    </div>
-                    <div align="right" className="">
-                      <a
-                        href="mailto:contact@vincipit.com"
-                        className="montserrat "
-                        rel="noreferrer"
-                        target="_blank"
-                      >
-                        Forgotten your password?
-                      </a>
-                    </div>
-                  </div>
-                  <div className="w-full flex justify-end mt-8">
-                    <div className="flex flex-col justify-center w-full">
-                      <button
-                        type="submit"
-                        className="dark:bg-[#313E68] bg-[#1473E6]  text-white px-4 py-2 rounded-xl w-full md:w-1/3 self-center md:self-end montserrat"
-                      >
-                        {t("common.submit")}
-                      </button>
-                    </div>
-                  </div>
-                </form>
+                </div>
               </div>
-            </div>
+            </form>
           </div>
         </div>
       </div>
     </div>
-  );
+  )
 };
 
 export default Login;
