c515293fab56b2a738153a80abf01aa310632024
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-20 16:58:10 +0200

    [add] loading page rewrite

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,7 +1,9 @@
 const LoadingPage = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
-      <h1>Loading stuff...</h1>
+    <div className="w-full h-screen fixed block z-50 top-0 right-0 left-0 bottom-0 overflow-hidden" style={{ background: "rgba(0,0,0,0.85)" }}>
+      <div className="flex flex-col justify-center items-center h-full w-full">
+        <h1>Loading stuff...</h1>
+      </div>
     </div>
   );
 };
