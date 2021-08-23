3b1289f7e3afa270c6e87e373544ec4afa624fc9
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-23 11:43:56 +0200

    [add] header horizontal and vertical depending on device (WIP)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,63 @@
-const { Link, useHistory } = require("react-router-dom");
-
+import { Link, useHistory } from "react-router-dom";
+import User from "../images/user.svg"
+import { useState } from "react";
 const Header = () => {
   const history = useHistory();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen)
+  }
+
+  const renderBurgerMenu = () => {
+    return (
+      <div className="flex flex-col h-screen absolute bg-[#292E41] text-white z-10 top-0 left-0" style={{ width: "40vw" }}>
+        <div className="h-12 md:h-20 flex items-center">
+          <img src="/vivi_white.svg" className="flex-shrink-0 m-3 h-12 w-auto" />
+          <button onClick={toggleOpen}>
+            menu
+          </button>
+        </div>
+        <div className="flex flex-col mt-4">
+          <Link to="/" onClick={toggleOpen}>
+            Home
+          </Link>
+          <Link to="/box" onClick={toggleOpen}>
+            Box
+          </Link>
+        </div>
+      </div>
+    )
+  }
+
+  const renderMenu = () => {
+    return (
+      <div className="w-full md:flex flex-row items-center h-full hidden">
+        <div className="flex justify-between ml-4">
+          <Link to="/">
+            Home
+          </Link>
+          <Link to="/box" className="ml-4">
+            Boxes
+          </Link>
+        </div>
+
+      </div>
+    )
+  }
   return (
-    <div
-      className="w-full bg-gray-400 dark:bg-gray-700 md:h-20 h-12 
-    flex items-center justify-between  dark:text-white"
-    >
-      <div className="h-full flex flex-col justify-center ml-2">
-        <img
-          src="/vivi_no-text.png"
-          alt="ViVi header logo"
-          className="h-1/3 w-auto cursor-pointer"
-          onClick={() => history.push("/")}
-        />
+    <header className="z-0">
+      <div
+        className="w-full bg-gray-400 dark:bg-[#292E41] h-12 md:h-20 dark:text-white flex items-center"
+      >
+        <img src="/vivi_white.svg" className="flex-shrink-0 m-3 md:m-6 h-12 w-auto" />
+        <button onClick={toggleOpen} className="block md:hidden">
+          menu
+        </button>
+        {!isOpen && renderMenu()}
       </div>
-      <div className="">
-        <Link to="/box">Boxes</Link>
-      </div>
-      <div className="w-1/3 md:w-1/6 flex justify-around mr-2">
-        <Link to="/profile">Settings</Link>
-        <span className="cursor-pointer">Sign Out</span>
-      </div>
-    </div>
+      {isOpen && renderBurgerMenu()}
+    </header>
   );
 };
 
