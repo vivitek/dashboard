601858810ca9bc7f7a157370710f388227a1497e
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-23 12:17:29 +0200

    [fix] header spacing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import { Link, useHistory } from "react-router-dom";
-import User from "../images/user.svg"
+import { Link, useLocation } from "react-router-dom";
 import { useState } from "react";
-const Header = () => {
-  const history = useHistory();
-  const [isOpen, setIsOpen] = useState(false);
 
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const Location = useLocation();
   const toggleOpen = () => {
     setIsOpen(!isOpen)
   }
@@ -13,7 +12,9 @@ const Header = () => {
     return (
       <div className="flex flex-col h-screen absolute bg-[#292E41] text-white z-10 top-0 left-0" style={{ minWidth: "40vw" }}>
         <div className="h-12 md:h-20 flex items-center">
-          <img src="/vivi_white.svg" className="flex-shrink-0 m-3 h-12 w-auto" />
+          <Link to="/">
+            <img src="/vivi_white.svg" className="flex-shrink-0 m-6 h-12 w-auto" />
+          </Link>
           <button onClick={toggleOpen}>
             menu
           </button>
@@ -32,8 +33,8 @@ const Header = () => {
 
   const renderMenu = () => {
     return (
-      <div className="w-full md:flex flex-row items-center h-full hidden">
-        <div className="flex justify-between ml-4">
+      <div className="w-full md:flex flex-row items-center h-full hidden ml-12">
+        <div className="flex justify-between">
           <Link to="/">
             Home
           </Link>
@@ -51,8 +52,10 @@ const Header = () => {
         className="w-full bg-gray-400 dark:bg-[#292E41] h-12 md:h-20 dark:text-white flex items-center"
       >
         {!isOpen && <>
-          <img src="/vivi_white.svg" className="flex-shrink-0 m-3 md:m-6 h-12 w-auto" />
-          <button onClick={toggleOpen} className="block lg:hidden">
+          <Link to="/">
+            <img src="/vivi_white.svg" className="flex-shrink-0 m-6 h-12 w-auto" />
+          </Link>
+          <button onClick={toggleOpen} className="block md:hidden">
             menu
           </button>
         </>}
