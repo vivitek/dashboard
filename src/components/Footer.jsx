8d72c3982a8e2a8f5d5fc9e491d723c751ad65e9
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-24 11:56:35 +0200

    [add] updating footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,19 +16,18 @@ const Footer = () => {
     <footer>
 
       <div className="w-full h-12 md:h-20 bg-gray-400 dark:bg-[#292E41] flex items-center justify-around dark:text-white">
-        <div className="h-full flex flex-col justify-center ml-2">
+        <div className="h-full flex items-center w-1/2">
           <img
             src="/vivi_no-text.png"
             alt="ViVi header logo"
-            className="h-1/3 w-auto cursor-pointer"
+            className="h-1/3 w-auto cursor-pointer ml-2"
             onClick={() => history.push("/")}
           />
+          <p className="ml-4">
+            &copy; {new Date().getFullYear()} <a href="mailto:contact@vincipit.com">Vincipit</a>
+          </p>
         </div>
         <div className="">
-          <p>
-            <span className="">&copy; {new Date().getFullYear()}</span>
-            <a href="mailto:contact@vincipit.com">Vincipit</a>
-          </p>
         </div>
         <div
           className="cursor-pointer text-black dark:text-white"
