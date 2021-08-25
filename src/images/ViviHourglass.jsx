ff5cb80e05075785e5707fb79d713ae470ae2e98
Matteo Gassend <matteo.gassend@epitech.eu>
2021-08-25 18:55:50 +0200

    [fix] fixing hourglass

--- src/images/ViviHourglass.jsx
+++ src/images/ViviHourglass.jsx
@@ -1,7 +1,30 @@
-const ViviHourglass = ({ className }) => {
+const ViviHourglass = ({ className, dark = true }) => {
+    if (dark) {
+        return (
+            <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100.27 112">
+                <defs>
+                </defs><g id="Calque_2" data-name="Calque 2">
+                    <g id="Calque_1-2" data-name="Calque 1">
+                        <path fill="#fff" d="M93.29,0H7a7,7,0,0,0-5.1,11.71L45,58.05a7,7,0,0,0,10.19,0L98.39,11.71A7,7,0,0,0,93.29,0Z" />
+                        <path fill="#fff" d="M55.27,74.74a7,7,0,0,0-10.58,0L22.63,100.51A7,7,0,0,0,27.92,112H72.05a7,7,0,0,0,5.28-11.49Z" /></g></g></svg>
 
+        )
+    }
     return (
-        <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100.15 112"><defs></defs><g id="Livello_2" data-name="Livello 2"><g id="Calque_1" data-name="Calque 1"><path className="cls-1" d="M50.12,55.77A2.49,2.49,0,0,1,48.31,55L5.16,8.64a2.46,2.46,0,0,1,.12-3.48A2.5,2.5,0,0,1,7,4.5H93.27a2.45,2.45,0,0,1,1.8,4.14L51.92,55A2.44,2.44,0,0,1,50.12,55.77Z" /><path className="cls-1" d="M88.59,9,50.12,50.32,11.64,9h77m4.68-9H6.92a7,7,0,0,0-5.1,11.71L45,58.05a7,7,0,0,0,9.9.29l.29-.29L98.33,11.71A7,7,0,0,0,93.23,0Z" /><path className="cls-1" d="M27.92,107.5a2.46,2.46,0,0,1-1.87-4.07L48.11,77.67a2.46,2.46,0,0,1,3.48-.27,1.72,1.72,0,0,1,.27.27l22.06,25.76a2.41,2.41,0,0,1,.36,2.64,2.36,2.36,0,0,1-2.23,1.43Z" /><path className="cls-1" d="M49.92,82.4,67.6,103H32.32l17.6-20.6m0-10.09a7,7,0,0,0-5.29,2.43L22.57,100.51A7,7,0,0,0,27.92,112H72.05a7,7,0,0,0,5.29-11.49L55.27,74.74A6.93,6.93,0,0,0,50,72.31Z" /></g></g></svg>
+        <svg className={className} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 100.27 112">
+            <defs>
+                <style>{".cls-1{ fill: url(#Dégradé_sans_nom_81);}.cls-2{ fill: url(#Dégradé_sans_nom_81-2);}"}</style>
+                <linearGradient id="Dégradé_sans_nom_81" x1="73.68" y1="47.76" x2="35.85" y2="-17.77" gradientUnits="userSpaceOnUse">
+                    <stop offset="0" stop-color="#65bfff" /><stop offset="0.12" stop-color="#56a4fb" />
+                    <stop offset="0.42" stop-color="#3164f3" /><stop offset="0.68" stop-color="#1736ec" />
+                    <stop offset="0.88" stop-color="#0619e8" /><stop offset="0.99" stop-color="#000ee7" />
+                </linearGradient>
+                <linearGradient id="Dégradé_sans_nom_81-2" x1="58.99" y1="120.63" x2="35.35" y2="79.68" xlinkHref="#Dégradé_sans_nom_81" />
+            </defs><g id="Calque_2" data-name="Calque 2"><g id="Calque_1-2" data-name="Calque 1">
+                <path class="cls-1" d="M93.29,0H7a7,7,0,0,0-5.1,11.71L45,58.05a7,7,0,0,0,10.19,0L98.39,11.71A7,7,0,0,0,93.29,0Z" />
+                <path class="cls-2" d="M55.27,74.74a7,7,0,0,0-10.58,0L22.63,100.51A7,7,0,0,0,27.92,112H72.05a7,7,0,0,0,5.28-11.49Z" /></g>
+            </g>
+        </svg>
     )
 }
 
