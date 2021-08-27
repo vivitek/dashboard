778fc1e0823e5a45c86c400f94da4de169ed389a
Matteo <matteo.gassend@epitech.eu>
2021-08-27 15:44:56 +0200

    [add] fix sizes for card

--- src/components/BoxCard.jsx
+++ src/components/BoxCard.jsx
@@ -2,27 +2,30 @@ import { useState, useEffect } from "react"
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
 import Settings from "../images/Settings";
+import ViviHourglass from "../images/ViviHourglass"
 const BoxCard = ({ data }) => {
-    const [color, setColor] = useState("#292E40");
+    const [color, setColor] = useState("#1A1F32");
     const [status, setStatus] = useState(false)
     const { t } = useTranslation();
 
 
     return (
-        <div className="flex flex-col" style={{ backgroundColor: color }}>
-            <div className="relative">
-                <img src="https://via.placeholder.com/50" alt="" />
-                <Settings className="fill-current stroke-current right-2 top-2 absolute h-6 w-auto" />
+        <div className="flex flex-col justify-evenly p-4 mx-4 mb-8 md:mb-3 lg:mx-0 md:h-80 md:w-80 w-full h-auto rounded-lg shadow-lg" style={{ backgroundColor: color }}>
+            <div className="flex justify-between mb-2">
+                <ViviHourglass dark={true} className="h-8 md:h-16 w-auto" />
+                <Settings className="fill-current stroke-current z-0 h-6 w-auto" />
             </div>
-            <div className="md:mt-3">
-                <h4><span className="capitalize">{t("boxCard.name")}</span>: {data.name}</h4>
+            <div className="mt-5 flex justify-between">
+                <h4 className="capitalize">{t("boxCard.name")}:</h4>
+                <p>{data.name}</p>
             </div>
-            <div className="md:mt-2">
-                <p><span className="capitalize">{t("boxCard.status")}</span>: {status ? t("boxCard.online") : t("boxCard.offline")} </p>
+            <div className="flex justify-between">
+                <h4 className="capitalize">{t("boxCard.status")}:</h4>
+                <p>{status ? t("boxCard.online") : t("boxCard.offline")}</p>
             </div>
-            <div className="self-end md:mt-5 mb-4 mr-2">
+            <div className="self-end mt-5">
                 <Link to={`/box/${data._id}`}
-                    className="dark:bg-[#313E68] bg-[#1473E6]  text-white px-6 py-2 rounded-lg font-medium  hover:bg-blue-600 transition duration-200 each-in-out font-sans"
+                    className="dark:bg-[#313E68] bg-[#1473E6] capitalize  text-white px-6 py-2 rounded-3xl font-medium  hover:bg-blue-600 transition duration-200 each-in-out font-sans"
                 >
                     {t("boxCard.details")}
                 </Link>
