7df9a763f1390de94f093a32467334f8fa392ab6
Matteo Gassend <matteo.gassend@epitech.eu>
2021-09-14 09:16:23 +0200

    [add] link to backend and information display

--- src/pages/Boxes_Details.jsx
+++ src/pages/Boxes_Details.jsx
@@ -1,10 +1,33 @@
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router";
+import { useQuery } from "@apollo/client";
+import { GET_ROUTER } from "../utils/apollo"
+import { useEffect, useState } from "react";
+import LoadingPage from "./Loading";
 
 const BoxDetails = () => {
-  const { t } = useTranslation()
-  //const { id } = useParams()
+  const { id } = useParams();
+  const { loading, error, data: routerData } = useQuery(GET_ROUTER, {
+    variables: { routerId: id },
+  });
+  const [isRouterOnline, setIsRouterOnline] = useState(false);
 
+  const { t } = useTranslation()
+
+  useEffect(() => {
+    if (routerData) {
+      console.log(routerData)
+      fetch(routerData.getRouter.url).then(() => {
+        setIsRouterOnline(true)
+      }).catch(() => {
+        setIsRouterOnline(false)
+      })
+    }
+  }, [routerData])
+
+  if (loading) {
+    return <LoadingPage />
+  }
 
   return (
     <div className="w-full h-full flex flex-col lg:flex-row py-4">
@@ -12,18 +35,18 @@ const BoxDetails = () => {
         <div className="dark:bg-darkBlue rounded-lg p-2 flex flex-col">
           <h3 className="font-itc uppercase font-medium">{t("boxDetails.information")}</h3>
           <div className="flex justify-between mt-2">
-            <h4>{t("boxDetails.name")}</h4>
-            <span>Somethign</span>
+            <h4 className="font-itc uppercase font-light">{t("boxDetails.name")}</h4>
+            <span>{routerData.getRouter.name}</span>
           </div>
           <div className="flex justify-between mt-1">
-            <h4>{t("boxDetails.name")}</h4>
-            <span>Somethign</span>
+            <h4 className="font-itc uppercase font-light">id</h4>
+            <span>{routerData.getRouter._id.substr(0, 5)}...</span>
           </div>
           <div className="flex justify-between mt-4">
-            <h4>{t("boxDetails.name")}</h4>
-            <span>Somethign</span>
+            <h4 className="font-itc uppercase font-light">{t("boxDetails.status")}</h4>
+            {isRouterOnline ? <div className="bg-green-500 h-4 w-4 rounded-full"></div> : <div className="bg-red-500 h-4 w-4 rounded-full"></div>}
           </div>
-          <div className="flex justify-start mt-4">
+          <div className="flex justify-evenly mt-4">
             <button>Off</button>
             <button>Reboot</button>
           </div>
