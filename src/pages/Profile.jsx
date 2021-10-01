6a368e240002bdb905b1fbc5517b72f0edfecc25
Matteo Gassend <matteo.gassend@epitech.eu>
2021-10-01 13:23:48 +0200

    [wip] started implementing 2fa activation and verification

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,15 @@
 import { useContext } from "react"
-import UserContext from "../contexts/userContext"
+import QrCode from "qrcode.react";
+import { /* useMutation, */ useQuery } from "@apollo/client";
 import avatarholder from 'avatarholder';
+import UserContext from "../contexts/userContext"
+import { /* TOGGLE_2FA, CHECK_2FA, */ GET_OTP_URL } from "../utils/apollo"
 
 const Profile = () => {
     const userContext = useContext(UserContext)
+    // const [checkToken] = useMutation(CHECK_2FA);
+    //const [toggleOtp] = useMutation(TOGGLE_2FA);
+    const { data: url_data } = useQuery(GET_OTP_URL);
 
     return (
         <div className="w-full h-full flex flex-col items-center justify-evenly">
@@ -29,6 +35,32 @@ const Profile = () => {
                         <h3>Account Activity</h3>
                         <p>coming soon...</p>
                     </div>
+                </div>
+            </div>
+            <div className="w-full md:w-1/2 space-y-4 md:spacey-0 flex flex-col md:flex-row justify-evenly bg-darkBlue p-6 rounded-lg">
+                <div>
+                    <div className="flex flex-col">
+                        <h3 className="font-semibold font-itc uppercase">preferences</h3>
+                        <div className="flex">
+                            <span>Language:</span>
+                            <select>
+
+                            </select>
+                        </div>
+                    </div>
+                    <div>
+                        <h3 className="font-semibold font-itc uppercase">password modification</h3>
+                        coming soon
+                    </div>
+                </div>
+                <div>
+                    <h3 className="font-semibold font-itc uppercase">2f authentication</h3>
+                    {url_data?.getOtpUrl && <div className="flex">
+                        <div>
+                            <QrCode href={url_data?.getOtpUrl} />
+                        </div>
+                    </div>}
+
                 </div>
             </div>
         </div>
